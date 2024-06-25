cc7bc2fc268ff73230dbe2f48e76c654aa1eaa18
MMLewis1225 <62947613+MMLewis1225@users.noreply.github.com>
2024-06-25 15:53:32 -0400

    added jar pressable icons

--- todo/components/HomeScreen/Jars.js
+++
@@ -1,3 +0,0 @@
-import React, { useEffect, useState } from "react";
-import { View, Text, ImageBackground, StyleSheet } from "react-native";
-export default function jars() {}
--- todo/components/HomeScreen/NoTask.js
+++ todo/components/HomeScreen/NoTask.js
@@ -1,22 +1,124 @@
-import React from "react";
-import { StyleSheet, View, Text, Pressable, Image } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, Text, Pressable, Image, Modal } from "react-native";
+import JarIcon from "../../components/SVGicons/JarIcon";
 
 export default function NoTask({ setTaskSelectionVisible, setModalVisible }) {
+  const [jarModalVisible, setJarModalVisible] = useState(false);
+  const [selectedJar, setSelectedJar] = useState(null);
+
+  const openJarModal = (jar) => {
+    setSelectedJar(jar);
+    setJarModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setJarModalVisible(false);
+    setSelectedJar(null); // Reset selectedJar when modal is closed
+  };
+
   return (
     <View style={styles.screen}>
-      <Text style={styles.welcomText}>No tasks active.</Text>
-      <Pressable
-        style={styles.pressableContainer}
-        onPress={() => setTaskSelectionVisible(true)}
-      >
-        <View>
-          <Text style={styles.buttonText}>Select a task</Text>
-          <Text style={styles.smallText}>
-            start with an easier task to gain motivation.
-          </Text>
+      <Text style={styles.welcomeText}>No tasks active.</Text>
+
+      <View style={styles.jarsContainer}>
+        {/* Container for Minutes */}
+        <View style={styles.jarContainer}>
+          <View style={styles.iconTextContainer}>
+            <Pressable
+              onPress={() => openJarModal("Minutes")}
+              style={({ pressed }) => [
+                { opacity: pressed || selectedJar === "Minutes" ? 0.6 : 1 },
+              ]}
+            >
+              <JarIcon
+                style={styles.jarIcon}
+                isActive={selectedJar === "Minutes"}
+              />
+            </Pressable>
+            <Text style={styles.jarText}>Minutes</Text>
+          </View>
         </View>
-      </Pressable>
-      <Pressable style={styles.addButton} onPress={() => setModalVisible(true)}>
+
+        {/* Container for Hours */}
+        <View style={styles.jarContainer}>
+          <View style={styles.iconTextContainer}>
+            <Pressable
+              onPress={() => openJarModal("Hours")}
+              style={({ pressed }) => [
+                { opacity: pressed || selectedJar === "Hours" ? 0.6 : 1 },
+              ]}
+            >
+              <JarIcon
+                style={styles.jarIcon}
+                isActive={selectedJar === "Hours"}
+              />
+            </Pressable>
+            <Text style={styles.jarText}>Hours</Text>
+          </View>
+        </View>
+
+        {/* Container for Days */}
+        <View style={styles.jarContainer}>
+          <View style={styles.iconTextContainer}>
+            <Pressable
+              onPress={() => openJarModal("Days")}
+              style={({ pressed }) => [
+                { opacity: pressed || selectedJar === "Days" ? 0.6 : 1 },
+              ]}
+            >
+              <JarIcon
+                style={styles.jarIcon}
+                isActive={selectedJar === "Days"}
+              />
+            </Pressable>
+            <Text style={styles.jarText}>Days</Text>
+          </View>
+        </View>
+      </View>
+
+      {/* Modal */}
+      <Modal
+        // animationType="slide"
+        transparent={true}
+        visible={jarModalVisible}
+        onRequestClose={closeModal}
+      >
+        <View style={styles.modalView}>
+          <View style={styles.modalContainer}>
+            {/* Close Modal Button */}
+            <Pressable style={styles.pressableContainer} onPress={closeModal}>
+              <Text style={styles.buttonText}>X</Text>
+            </Pressable>
+
+            {/* Choose Random Task Button */}
+            <Pressable
+              style={styles.pressableContainer}
+              onPress={() => {
+                closeModal();
+                setTaskSelectionVisible(true);
+              }}
+            >
+              <Text style={styles.buttonText}>Choose random task</Text>
+            </Pressable>
+
+            {/* Choose Task Button */}
+            <Pressable
+              style={styles.pressableContainer}
+              onPress={() => {
+                closeModal();
+                setTaskSelectionVisible(true);
+              }}
+            >
+              <Text style={styles.buttonText}>Choose task</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      {/* Add Button */}
+      <Pressable
+        style={styles.addTaskButton}
+        onPress={() => setModalVisible(true)}
+      >
         <Image
           style={{ width: 90, height: 90 }}
           source={require("../../assets/addButton.png")}
@@ -25,60 +127,86 @@ export default function NoTask({ setTaskSelectionVisible, setModalVisible }) {
     </View>
   );
 }
+
 const styles = StyleSheet.create({
-  addButton: {
+  container: {
+    backgroundColor: "#48249c",
+    textAlign: "center",
+    borderRadius: 20,
+    marginBottom: 10,
+    alignItems: "center",
+    padding: 10,
+  },
+  addTaskButton: {
     position: "absolute",
     bottom: 20,
     right: 20,
   },
   screen: {
     flex: 1,
-    // backgroundColor: "#232138",
-    // padding: 10,
     justifyContent: "center",
+    alignItems: "center",
   },
-
-  smallText: {
-    fontStyle: "italic",
-    fontFamily: "Avenir-Book",
-    marginBottom: 20,
-    fontSize: 18,
-    color: "white",
-  },
-
   pressableContainer: {
     backgroundColor: "#48249c",
     textAlign: "center",
     borderRadius: 20,
     marginBottom: 10,
     alignItems: "center",
+    padding: 10,
   },
-
-  welcomText: {
-    display: "flex",
-    alignItems: "center",
-    gap: 6,
+  welcomeText: {
     fontSize: 30,
-    marginTop: 12,
-    marginLeft: 30,
-    marginRight: 30,
-    marginBottom: 50,
-    fontWeight: "bold",
+    // fontWeight: "bold",
     fontFamily: "Poppins",
     textAlign: "center",
-    //color: 'white'
+    marginTop: 12,
+    marginBottom: 50,
   },
   buttonText: {
-    display: "flex",
-    alignItems: "center",
-    gap: 6,
     fontSize: 26,
-    marginTop: 12,
-    marginLeft: 8,
-    marginBottom: 5,
     fontWeight: "bold",
     fontFamily: "Poppins",
-    textAlign: "center",
     color: "white",
+    marginBottom: 5,
+  },
+  modalContainer: {
+    padding: 40,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "flex-start",
+
+    borderRadius: 10,
+  },
+
+  modalView: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.7)",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  jarsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
+    paddingHorizontal: 20,
+  },
+  jarContainer: {
+    alignItems: "center",
+  },
+  iconTextContainer: {
+    position: "relative",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  jarIcon: {
+    width: 145,
+    height: 167,
+  },
+  jarText: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center",
+    position: "absolute",
   },
 });
