0ff0770d3bd5d2aa22befbae0a96a6baec2bc2f7
MMLewis1225 <62947613+MMLewis1225@users.noreply.github.com>
2024-07-01 16:15:04 -0400

    got active task formatted better, need to add darker opacity behind it

--- todo/components/HomeScreen/CurrentTask.js
+++ todo/components/HomeScreen/CurrentTask.js
@@ -1,46 +1,44 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Animated, Dimensions, Modal, ScrollView} from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Animated, Dimensions, Modal, ScrollView } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { updateTodo } from '../TodosService';
 import { useNavigation } from '@react-navigation/native';
 import { AntDesign } from '@expo/vector-icons';
 import VerticalLine from "../SVGicons/VerticalLine";
+
 const windowHeight = Dimensions.get('window').height;
 
-export default function CurrentTask({
- visible,
- currentTask,
- setCurrentTask,
-}) {
- const navigation = useNavigation();
- const [modalVisible, setModalVisible] = useState(false);
- const translateY = useState(new Animated.Value(-windowHeight))[0];
+export default function CurrentTask({ visible, currentTask, setCurrentTask }) {
+  const navigation = useNavigation();
+  const [modalVisible, setModalVisible] = useState(false);
+  const translateY = useState(new Animated.Value(-windowHeight))[0];
 
- const openModal = () => {
-   setModalVisible(true);
-   Animated.timing(translateY, {
-     toValue: -200,
-     duration: 300,
-     useNativeDriver: true,
-   }).start();
- };
+  const openModal = () => {
+    setModalVisible(true);
+    Animated.timing(translateY, {
+      toValue: -200,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
 
- const closeModal = () => {
-   Animated.timing(translateY, {
-     toValue: -windowHeight,
-     duration: 300,
-     useNativeDriver: true,
-   }).start(() => setModalVisible(false));
- };
+  const closeModal = () => {
+    Animated.timing(translateY, {
+      toValue: -windowHeight,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(() => setModalVisible(false));
+  };
 
- const handleFinishTask = async () => {
-   currentTask.completed = true;
-   currentTask.completion_date = Date.now();
-   await updateTodo(currentTask.key, currentTask);
-   setCurrentTask({});
-   closeModal(); // Close modal after finishing task
-   navigation.navigate('Activity'); // Navigate back to the Activity component
- };
+  const handleFinishTask = async () => {
+    currentTask.completed = true;
+    currentTask.completion_date = Date.now();
+    await updateTodo(currentTask.key, currentTask);
+    setCurrentTask({});
+    closeModal(); // Close modal after finishing task
+    navigation.navigate('Activity'); // Navigate back to the Activity component
+  };
+
   const completeTask = async () => {
     currentTask.completed = true;
     currentTask.completion_date = Date.now();
@@ -49,114 +47,130 @@ export default function CurrentTask({
     navigation.navigate("Activity"); // Navigate back to the Activity component
   };
 
- return (
-   <Modal visible={visible} transparent={true}>
-     <View style={styles.screen}>
-
- {/* opens the exit task modal. Should be positioned absolute somewhere above taskcard.*/}
+  return (
+    <Modal visible={visible} transparent={true}>
+      <View style={styles.screen}>
+        {/* opens the exit task modal. Should be positioned absolute somewhere above taskcard.*/}
         <TouchableOpacity onPress={openModal}>
-           <View style={styles.exitTaskArrow}>
-             <AntDesign name="downcircle" size={24} color="white" />
-           </View>
+          <View style={styles.exitTaskArrow}>
+            <AntDesign name="downcircle" size={40} color="white" />
+          </View>
         </TouchableOpacity>
 
-       <View style={styles.curTask}>
-              {/* */}
- <ScrollView style={styles.scroll}>
-         <Text style={styles.welcomeText}>Active Task</Text>
-         <VerticalLine/>
+        <View style={styles.curTask}>
+          <ScrollView style={styles.scroll}>
+            <Text style={styles.welcomeText}>Active Task</Text>
+            <VerticalLine />
+            <Text style={styles.taskText}>{currentTask.text}</Text>
+          </ScrollView>
 
-         <Text style={styles.taskText}>{currentTask.text}</Text>
+          {/* Custom animated modal */}
+          <Animated.View style={[styles.modalContainer, { transform: [{ translateY }] }]}>
+            <View style={styles.modalContent}>
+              <View style={styles.closeIconContainer}>
+                {/* Close icon */}
+                <TouchableOpacity onPress={() => setCurrentTask({})}>
+                  <FontAwesome name="close" style={styles.closeIcon} size={40} />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.modalItem}>
+                {/* Finished Task button */}
+                <TouchableOpacity onPress={completeTask}>
+                  <Text style={styles.finishedTaskButton}>Finished Task</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.modalItem}>
+                {/* upcircle, moves modal back up */}
+                <TouchableOpacity onPress={closeModal}>
+                  <AntDesign name="upcircle" size={40} color="black" />
+                </TouchableOpacity>
+              </View>
 
-  </ScrollView>
 
-         {/* Custom animated modal */}
-
-         <Animated.View style={[styles.modalContainer, { transform: [{ translateY }] }]}>
-           <View style={styles.modalContent}>
-
-            {/* Close icon */}
-                      <TouchableOpacity onPress={() => setCurrentTask({})}>
-                        <FontAwesome name="close" style={styles.closeIcon} size={40} />
-                      </TouchableOpacity>
-
-     {/* Finished Task button */}
-                                            <TouchableOpacity onPress={completeTask}>
-                                              <Text>Finished Task</Text>
-                                            </TouchableOpacity>
-     {/* upcircle, moves modal back up  */}
-             <TouchableOpacity onPress={closeModal}>
-                 <AntDesign name="upcircle" size={24} color="black" />
-             </TouchableOpacity>
-
-           </View>
-         </Animated.View>
+            </View>
+          </Animated.View>
           {/* end Custom animated modal */}
-
-       </View>
-     </View>
-   </Modal>
- );
+        </View>
+      </View>
+    </Modal>
+  );
 }
 
 const styles = StyleSheet.create({
- screen: {
-   flex: 1,
-   justifyContent: 'center',
-   backgroundColor: 'rgba(0, 0, 0, 0.7)',
- },
-  //should be positioned above curTask
-  exitTaskArrow: {
-       //position: "absolute",
-      // bottom: 50,
-     //  right: 20,
-   //  marginBottom: 20,
-    alignItems: 'flex-end',
-   //position: "absolute",
-  // left: 286,
-  // top: 96,
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
   },
- curTask: {
-   backgroundColor:  "white", //'#EBEBEB',
-   padding: 24,
-     //width: "100%",
-   //  position: "absolute",
-   //  justifyContent: "center",
-   height: 400,
- },
- welcomeText: {
-   fontSize: 32,
-   color: '#BEBEBE',
-   textAlign: 'left',
-   marginBottom: 10,
- },
- taskText: {
- fontSize: 24,
- color: 'black',
- marginBottom: 20,
- marginTop: 30,
- },
-    scroll: {
-      marginTop: 16,
-    },
- modalContainer: {
-   position: 'absolute',
-   top: 0,
-   left: 0,
-   right: 0,
-   justifyContent: 'flex-start',
-   alignItems: 'center',
-  // backgroundColor: "red",
- },
- modalContent: {
-   width: '100%',
-   height: windowHeight / 3,
-  backgroundColor: '#F5FCFF',
-   padding: 20,
-   alignItems: 'center',
- },
+  // should be positioned above curTask
+  exitTaskArrow: {
+    position: "absolute",
+    bottom: 70,
+    alignItems: 'flex-end',
+  },
+  curTask: {
+    backgroundColor: "white", //'#EBEBEB',
+    padding: 24,
+    height: 400,
+  },
+  welcomeText: {
+    fontSize: 32,
+    color: '#BEBEBE',
+    textAlign: 'left',
+    marginBottom: 10,
+  },
+  taskText: {
+    fontSize: 24,
+    color: 'black',
+    marginBottom: 20,
+    marginTop: 30,
+  },
+  scroll: {
+    marginTop: 16,
+  },
+  modalContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  modalContent: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: "white",
+    padding: 20,
+    height: 400,
+  },
+  closeIconContainer: {
+    alignItems: "flex-start",
+    width: "100%",
+  },
+  modalItem: {
+    alignItems: "center",
+    width: "100%",
+    marginVertical: 10,
+  },
   closeIcon: {
+    alignSelf: "flex-start",
     color: 'black',
     marginBottom: 10,
+  },
+  finishedTaskButton: {
+    backgroundColor: "black",
+    color: "white",
+    borderRadius: 8,
+    padding: 10,
+    fontSize: 18,
+    textAlign: "center",
+    marginTop: 10,
+  },
+  modalSetOpacity: {
+  position: "absolute",
+       height: 1000,
+       backgroundColor: 'rgba(0, 0, 0, 0.7)',
   },
 });
