8cc847955a9177ebf0882fec5be4ba0242e8c55d
Maria Molchanova <moshimasha@gmail.com>
2024-07-03 18:30:31 -0400

    dailygoal fix

--- todo/components/ActivityScreen/DailyGoal.js
+++ todo/components/ActivityScreen/DailyGoal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { useFocusEffect } from '@react-navigation/native';Z
+import { useFocusEffect } from '@react-navigation/native';
 const DailyGoal = () => {
     return (
         <View>
