5ca099b30bc6cf5dfc1d66126ad810c05307e743
MMLewis1225 <62947613+MMLewis1225@users.noreply.github.com>
2024-07-05 00:38:11 -0400

    need to make edit task look look add task

--- todo/components/EditTaskModal.js
+++ todo/components/EditTaskModal.js
@@ -1,56 +1,38 @@
 import React from "react";
 import { StyleSheet, View, Modal } from "react-native";
-import EditTask from "./EditTask.js";
+import EditTask from "./EditTask";
 
-export default function EditTaskModal({ modalVisible, setModalVisible, task}) {
-
-
+export default function EditTaskModal({ modalVisible, setModalVisible, task }) {
   return (
-  <>
-    <Modal transparent={true}  animationType="slide" visible={modalVisible} style={styles.modalView}>
-
-          <View style={styles.modalContainer}>
-                              <View style={styles.modalContent}>
-                              <EditTask setModalVisible={setModalVisible} task = {task} />
-                              </View>
-                            </View>
+    <Modal transparent={true} animationType="slide" visible={modalVisible}>
+      <View style={styles.modalView}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <EditTask setModalVisible={setModalVisible} task={task} />
+          </View>
+        </View>
+      </View>
     </Modal>
-{/*
-            <Modal visible={true} animationType="slide" transparent={true}>
-                      <View style={styles.modalContainer}>
-                        <View style={styles.modalContent}>
-                        </View>
-                      </View>
-                    </Modal>
-
-*/}
-    </>
   );
 }
 
 const styles = StyleSheet.create({
   modalView: {
-    //margin: 20,
-    //borderRadius: 20,
-    //padding: 35,
-    // alignItems: 'center',
     flex: 1,
-    //backgroundColor: 'transparent',
-    backgroundColor: "rgba(0,0,0,0.7)",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
-    modalContainer: {
-      flex: 1,
-      justifyContent: "flex-end",
-      alignItems: "center",
-      backgroundColor: "rgba(0, 0, 0, 0.5)",
-    },
-    modalContent: {
-      width: "95%",
-      height: "95%",
-      backgroundColor: "#EBEBEB",
-      borderRadius: 10,
-//      padding: 20,
-    },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  modalContent: {
+    width: "100%",
+    backgroundColor: "white",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    padding: 20,
+  },
 });
