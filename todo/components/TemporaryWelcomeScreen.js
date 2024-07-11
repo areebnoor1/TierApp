981f3fe632de3476b5b055626a35d0056cb38315
areebnoor1 <wyt4at@virginia.edu>
2024-07-10 21:00:35 -0400

    updated loading screen

--- todo/components/TemporaryWelcomeScreen.js
+++ todo/components/TemporaryWelcomeScreen.js
@@ -1,11 +1,14 @@
-// components/TemporaryWelcomeScreen.js
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 const TemporaryWelcomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome to the App!</Text>
+      <Text style={styles.text}>Welcome to TaskJar</Text>
+      <Image
+        source={require('../assets/landingjar.png')}
+        style={styles.logo}
+      />
     </View>
   );
 };
@@ -15,11 +18,18 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#fff', // Ensure the background color is set
+    backgroundColor: '#a8cccc', // Set the background color
   },
   text: {
     fontSize: 24,
     fontWeight: 'bold',
+    position: 'absolute',
+    top: '20%', // Move the text further up
+  },
+  logo: {
+    width: 150, // Adjust the size as needed
+    height: 150,
+    resizeMode: 'contain',
   },
 });
 
